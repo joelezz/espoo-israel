19c632ac1b70369967926db67c0b2ddeda069ce3
unknown <joel.ezzahid@gmail.com>
2024-09-16 15:40:28 +0300

    new commit

--- main.py
+++ main.py
@@ -1,60 +1,61 @@
 from flask import Flask, request, redirect, url_for, render_template, flash
 from flask_mail import Mail, Message
 import secrets, os
+from dotenv import load_dotenv
+from flask_wtf.csrf import CSRFProtect
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_wtf.csrf import CSRFProtect
+from contact_form import ContactForm
+
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
+load_dotenv()
+# The secret key for session management
+app.secret_key = secrets.token_hex(16)
 
 # Flask-Mail configurations
 app.config['MAIL_SERVER']='daxpower.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'lomakkeet@daxpower.com'
 app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')
-print(f"EMAIL_PASSWORD: {os.environ.get('EMAIL_PASSWORD')}")
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-
-# The secret key for session management
-app.secret_key = secrets.token_hex(16)
+app.config['SECRET_KEY'] = app.secret_key
 
 # Initialized Flask-Mail
 mail = Mail(app)
 
 # Home route to serve the HTML form
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')  # Your HTML page with the form
+    form = ContactForm()  # Your custom form
+    if form.validate_on_submit():
+        # Retrieve form data
+        name = form.name.data
+        email = form.email.data
+        message = form.message.data
 
-# Route to handle form submission
-@app.route('/submit-form', methods=['POST'])
-def submit_form():
-    # Retrieve form data
-    name = request.form.get('name')
-    email = request.form.get('email')
-    message = request.form.get('message')
+        # Send email
+        try:
+            msg = Message(f"New Contact Form Submission from {name}",
+                          sender='lomakkeet@daxpower.com',  # Sender email
+                          recipients=["zzjoe@tuta.io"])  # Recipient email
+            msg.body = f"Name: {name}\nEmail: {email}\nMessage: {message}"
+            mail.send(msg)
+            flash("Viestisi lähetettiin onnistuneesti!")
+            return redirect(url_for('thank_you'))
+        except Exception as e:
+            print(f"Viestin toimitus ei onnistunut: {str(e)}")
+            flash("Viestin toimitus ei onnistunut, kokeile uudestaan hetken kuluttua.")
+            return redirect(url_for('home'))
 
-    # Validated form data
-    if not name or not email or not message:
-        flash("All fields are required!")
-        return redirect(url_for('home'))
-
-    # Send email
-    try:
-        msg = Message(f"New Contact Form Submission from {name}",
-                      sender='lomakkeet@daxpower.com',  # Sender email (your email)
-                      recipients=["joe@daxpower.com"])  # Recipient email
-        msg.body = f"Name: {name}\nEmail: {email}\nMessage: {message}"
-        mail.send(msg)
-        flash("Your message has been sent successfully!")
-        return redirect(url_for('thank_you'))
-    except Exception as e:
-        print(f"Failed to send email: {str(e)}")
-        flash("Failed to send the message. Please try again later.")
-        return redirect(url_for('home'))
+    return render_template('index.html', form=form)
 
 # Thank you route
-@app.route('/thank-you')
+@app.route('/kiitos')
 def thank_you():
-    return "Thank you for contacting us! We will get back to you shortly."
+    return "Kiitos yhteydenotosta! Palaamme sinulle mahdollisimman pian."
 
 if __name__ == '__main__':
     app.run()

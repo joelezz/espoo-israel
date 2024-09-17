d3bb2df23b14b4a8e037e1452c3c55fb4c3a5322
unknown <joel.ezzahid@gmail.com>
2024-09-17 19:25:58 +0300

    fixed csrf form issue

--- contact_form.py
+++ contact_form.py
@@ -3,7 +3,7 @@ from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 
 class ContactForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
-    message = TextAreaField('Message', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    name = StringField('Yrityksen nimi ja yhteyshenkilö:', validators=[DataRequired()])
+    email = StringField('Sähköposti:', validators=[DataRequired()])
+    message = TextAreaField('Viesti:', validators=[DataRequired()])
+    submit = SubmitField('Lähetä')

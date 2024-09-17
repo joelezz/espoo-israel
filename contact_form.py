0684ed76d1c6abf7e42c5431a95491faf0ba2d07
unknown <joel.ezzahid@gmail.com>
2024-09-17 21:19:23 +0300

    form validators updated

--- contact_form.py
+++ contact_form.py
@@ -1,9 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
+
 
 class ContactForm(FlaskForm):
     name = StringField('Yrityksen nimi ja yhteyshenkilö:', validators=[DataRequired()])
-    email = StringField('Sähköposti:', validators=[DataRequired()])
+    email = StringField('Sähköposti', validators=[DataRequired(), Email()])
     message = TextAreaField('Viesti:', validators=[DataRequired()])
     submit = SubmitField('Lähetä')
